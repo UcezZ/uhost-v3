2316b29cb6b82393aec5dc36ef215f13c2242d88
UcezZ <ucezz@mail.ru>
2023-09-28 23:22:42 +0300

    LF

--- Docker/pginit.sh
+++ Docker/pginit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# KEEP THIS FILE LF-LINEBREAKED!!!
 
 set -e
 
@@ -21,7 +22,3 @@ echo "pg_trgm.word_similarity_threshold = 0.3" >> $CONF_FILE
 
 # Создание БД Sentry
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE $POSTGRES_DB_SENTRY;"
-# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB_SENTRY" -c "CREATE SCHEMA IF NOT EXISTS $POSTGRES_DB_SCHEMA;"
-# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB_SENTRY" -c "ALTER SCHEMA $POSTGRES_DB_SCHEMA OWNER TO $POSTGRES_USER;"
-# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB_SENTRY" -c "ALTER DATABASE $POSTGRES_DB_SENTRY SET SEARCH_PATH TO $POSTGRES_DB_SCHEMA, public;"
-# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB_SENTRY" -c "SET SEARCH_PATH TO $POSTGRES_DB_SCHEMA, public;"
