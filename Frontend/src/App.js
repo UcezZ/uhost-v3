9895f25af07f0919d160ad7b34a5f6d029b465d5
UcezZ <ucezz@mail.ru>
2023-10-09 01:27:16 +0300

    docker, paths

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,6 +4,7 @@ import AuthPage from "./components/pages/AuthPage";
 import StateContext from './context/StateContext';
 import ErrorDialog from './components/ErrorDialog';
 import Header from "./components/Header";
+import config from './config.json';
 
 export default function App() {
   const [theme, setTheme] = useState('dark');
@@ -18,8 +19,8 @@ export default function App() {
       <Header />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<div />} />
-          <Route path="/login" element={<AuthPage />} />
+          <Route path={`${config.webroot}/`} element={<Link to={`${config.webroot}/login`}>login</Link>} />
+          <Route path={`${config.webroot}/login`} element={<AuthPage />} />
         </Routes>
       </BrowserRouter>
       <ErrorDialog />
