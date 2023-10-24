5165e087950cab583be803990cdd566b2eefa403
UcezZ <ucezz@mail.ru>
2023-10-24 23:20:34 +0300

    async crutch

--- Uhost.Console/Commands/TaskExecutorCommand.cs
+++ Uhost.Console/Commands/TaskExecutorCommand.cs
@@ -22,29 +22,37 @@ namespace Uhost.Console.Commands
         public string Queue { get; set; }
 
         private static bool _cancel;
-        private static LogWriter _logger;
+        private LogWriter _logger;
+        private readonly SemaphoreSlim _semaphore;
+
+        public TaskExecutorCommand()
+        {
+            _semaphore = new SemaphoreSlim(ConsoleSettings.TaskExecutorWorkThreads);
+        }
 
         public override void Run()
         {
+            _logger = GetRequiredService<LogWriter>();
             var service = GetRequiredService<IQueueService>();
             var q = service.RegisterQueue(Queue);
-            service.ConsumingChannel.BasicQos(0, ConsoleSettings.TaskExecutorWorkThreads, true);
-
-            _logger = GetRequiredService<LogWriter>();
+            service.Channel.BasicQos(0, ConsoleSettings.TaskExecutorWorkThreads, false);
 
             CancelKeyPress += OnCancelKeyPress;
 
-            var consumer = new AsyncEventingBasicConsumer(service.Channel);
+            for (var i = 0; i < ConsoleSettings.TaskExecutorWorkThreads; i++)
+            {
+                var consumer = new AsyncEventingBasicConsumer(service.Channel);
 
-            consumer.Received += OnReceivedAsync;
+                consumer.Received += OnReceivedAsync;
 
-            var tk = service.Channel.BasicConsume(
-                queue: q.QueueName,
-                autoAck: true,
-                consumerTag: $"worker-{Queue}",
-                consumer: consumer);
+                var tk = service.Channel.BasicConsume(
+                    queue: q.QueueName,
+                    autoAck: true,
+                    consumerTag: $"worker-{Queue}-{i}",
+                    consumer: consumer);
 
-            _logger.WriteLine($"Started consumer with tag \"{tk}\"", Severity.Info);
+                _logger.WriteLine($"Started consumer with tag \"{tk}\"", Severity.Info);
+            }
 
             while (!_cancel)
             {
@@ -58,31 +66,46 @@ namespace Uhost.Console.Commands
             e.Cancel = true;
         }
 
+        private async Task ProcessOneTask(string message, BasicDeliverEventArgs e)
+        {
+            await _semaphore.WaitAsync();
+            try
+            {
+                await _logger.WriteLineAsync($"[{e.ConsumerTag}] started processing #{e.DeliveryTag}", Severity.Info);
+
+                if (SerializedTask.TryParseJson(message, out var task))
+                {
+                    try
+                    {
+                        await task.InvokeAsync(GetRequiredService);
+                    }
+                    catch (Exception exception)
+                    {
+                        SentrySdk.CaptureException(exception);
+                        var inner = exception.GetMostInnerException();
+                        await _logger.WriteLineAsync($"Error processing task {e.DeliveryTag}: {inner?.Message}\r\n{inner?.StackTrace}", Severity.Error);
+                    }
+
+                    await _logger.WriteLineAsync($"[{e.ConsumerTag}] task #{e.DeliveryTag} completed", Severity.Info);
+                }
+                else
+                {
+                    await _logger.WriteLineAsync($"[{e.ConsumerTag}] the received message #{e.DeliveryTag} is not a task", Severity.Warn);
+                }
+                Thread.Sleep(3000);
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
         private async Task OnReceivedAsync(object model, BasicDeliverEventArgs e)
         {
             var message = Encoding.UTF8.GetString(e.Body.Span);
-
             await _logger.WriteLineAsync($"[{e.ConsumerTag}] received a message #{e.DeliveryTag}", Severity.Info);
-
-            if (SerializedTask.TryParseJson(message, out var task))
-            {
-                try
-                {
-                    await task.InvokeAsync(GetRequiredService);
-                }
-                catch (Exception exception)
-                {
-                    SentrySdk.CaptureException(exception);
-                    var inner = exception.GetMostInnerException();
-                    await _logger.WriteLineAsync($"Error processing task {e.DeliveryTag}: {inner?.Message}\r\n{inner?.StackTrace}", Severity.Error);
-                }
-
-                await _logger.WriteLineAsync($"[{e.ConsumerTag}] task #{e.DeliveryTag} completed", Severity.Info);
-            }
-            else
-            {
-                await _logger.WriteLineAsync($"[{e.ConsumerTag}] the received message #{e.DeliveryTag} is not a task", Severity.Warn);
-            }
+            var thread = new Thread(async () => await ProcessOneTask(message, e));
+            thread.Start();
         }
     }
 }
