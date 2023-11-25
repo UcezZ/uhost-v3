eca92b79c41e854123584e63bb47494c2671099c
UcezZ <ucezz@mail.ru>
2023-11-25 13:18:43 +0300

    exception

--- Uhost.Web/Controllers/ExceptionController.cs
+++ Uhost.Web/Controllers/ExceptionController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
+using Sentry;
 using System;
 using System.Net;
 using Uhost.Core.Extensions;
@@ -41,8 +42,24 @@ namespace Uhost.Web.Controllers
         /// </summary>
         /// <param name="message">Текст</param>
         /// <returns></returns>
-        [HttpGet("api/v2/exception"), Authorize]
-        public IActionResult Exception(string message) =>
+        [HttpPost("api/v2/exception"), Authorize]
+        public IActionResult Exception([FromForm] string message)
+        {
             throw new Exception(message);
+        }
+
+        /// <summary>
+        /// Отправляет исключение в Sentry
+        /// </summary>
+        /// <example>
+        /// Не бросает исключение, только создаёт его и отправляет в Sentry
+        /// </example>
+        /// <param name="message">Текст</param>
+        /// <returns></returns>
+        [HttpPost("api/v2/exception/sentry"), Authorize]
+        public IActionResult ExceptionSentry([FromForm] string message)
+        {
+            return ResponseHelper.Success(SentrySdk.CaptureException(new Exception(message)));
+        }
     }
 }
