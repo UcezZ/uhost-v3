08732ec7bfbb947e9d38a3a93cd0dca51ba025f6
UcezZ <ucezz@mail.ru>
2024-02-24 20:20:04 +0300

    docker fix

--- Docker/nginx/entrypoint.sh
+++ Docker/nginx/entrypoint.sh
@@ -3,6 +3,7 @@
 export REDIS_ADDR=$(dig +short redis) 
 export REDIS_DBNUM=$REDIS_DBNUM 
 
-echo $REDIS_ADDR $REDIS_DBNUM
+echo Redis address: $REDIS_ADDR 
+echo Redis DB: $REDIS_DBNUM
 
 env REDIS_ADDR=$REDIS_ADDR /usr/local/nginx/sbin/nginx -g 'daemon off;'
