0208493ee0f22579d997f6fe21f90f60f2321663
UcezZ <ucezz@mail.ru>
2024-04-08 00:28:11 +0300

    pg

--- Docker/pginit.sh
+++ Docker/pginit.sh
@@ -15,6 +15,13 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE EXTENSION IF NOT EXISTS btree_gin;"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "SELECT * FROM pg_extension;"
 
+# tgrm
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "ALTER SYSTEM SET pg_trgm.similarity_threshold = 0.1;"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "ALTER SYSTEM SET pg_trgm.word_similarity_threshold = 0.1;"
+
+# TZ
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "ALTER SYSTEM SET timezone = '+03:00';"
+
 # Создание БД логов
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE $POSTGRES_DB_LOGS;"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB_LOGS" -c "ALTER DATABASE $POSTGRES_DB_LOGS OWNER TO $POSTGRES_USER;"
@@ -22,3 +29,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB_LOGS"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB_LOGS" -c "ALTER SCHEMA $POSTGRES_DB_SCHEMA OWNER TO $POSTGRES_USER;"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB_LOGS" -c "ALTER DATABASE $POSTGRES_DB_LOGS SET SEARCH_PATH TO $POSTGRES_DB_SCHEMA, public;"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB_LOGS" -c "SET SEARCH_PATH TO $POSTGRES_DB_SCHEMA, public;"
+
+# TZ
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB_LOGS" -c "ALTER SYSTEM SET timezone = '+03:00';"
